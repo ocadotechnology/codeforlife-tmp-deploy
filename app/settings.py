ccf35b7faa3c41ef7388ef3c85b38be3862d5fce
SKairinos <stefan.kairinos@ocado.com>
2025-02-05 15:11:55 +0000

    todo

--- app/settings.py
+++ app/settings.py
@@ -1,2 +1,5 @@
 # TODO: connect to db like so:
 # https://github.com/ocadotechnology/codeforlife-package-python/blob/75aca376cedcffcdc3d6e110736358289f30e9db/codeforlife/settings/django.py#L57
+
+# TODO: download and load secrets as namespace
+# https://github.com/ocadotechnology/codeforlife-package-python/blob/75aca376cedcffcdc3d6e110736358289f30e9db/codeforlife/__init__.py#L113
